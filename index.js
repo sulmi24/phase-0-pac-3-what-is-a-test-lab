4b095e6443fe98d15c84206b6d7b3d2f9255cb54
sulmi24 <a.sulmi24@gmail.com>
2023-02-11 10:58:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const name = "Joe";
-const height = 74;
+const name = "Susan";
+const height = 39;
+describe("Height", () => {
+  it("is less than 40", () => {
+    expect(height).toBeLessThan(40);
+  });
+});
 const message = `${name} is ${height} inches tall`;
 
-module.exports = {name, height, message};
+module.exports = { name, height, message };
